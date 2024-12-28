74253bec6a02d13442b691633109935e5c41719e
JeongHunHui <deca3248@gmail.com>
2024-12-28 20:27:59 +0900

    feat: 테스트를 위한 ping API와 error를 반환하는 error API 구현

---
+++ api/src/main/kotlin/com/hunhui/api/controller/SampleController.kt
@@ -0,0 +1,22 @@
+package com.hunhui.api.controller
+
+import com.hunhui.api.controller.doc.SampleApiDoc
+import com.hunhui.api.controller.exception.SampleException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleController : SampleApiDoc {
+    @GetMapping("/ping")
+    override fun ping(): ResponseEntity<SampleResponse> = ResponseEntity.ok(SampleResponse("pong"))
+
+    @GetMapping("/error")
+    override fun error() {
+        throw SampleException()
+    }
+
+    data class SampleResponse(
+        val message: String
+    )
+}
