7930d3d8b7f651a435b1227f31ef89031ab4a4c5
Octoping <myc1365@naver.com>
2024-12-28 22:43:04 +0900

    Update Username.kt

--- src/main/kotlin/octoping/scenariostudy/domain/users/model/Username.kt
+++ src/main/kotlin/octoping/scenariostudy/domain/users/model/Username.kt
@@ -1,12 +1,9 @@
 package octoping.scenariostudy.domain.users.model
 
 data class Username(
-    private val _value: String,
+    val value: String,
 ) {
     init {
-        require(_value.length >= 2) { "사용자 이름은 2글자 이상이어야 합니다" }
+        require(value.length >= 2) { "사용자 이름은 2글자 이상이어야 합니다" }
     }
-
-    val value: String
-        get() = _value
 }
