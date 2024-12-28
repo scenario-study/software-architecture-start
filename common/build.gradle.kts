05bd8f827e4593319912ad8308aa50e0fc58e303
CodeDiary18 <codediary18@gmail.com>
2024-12-29 01:21:37 +0900

    chore: delete lombok dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -16,8 +16,6 @@ tasks.bootJar {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
